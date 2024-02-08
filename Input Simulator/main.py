704f64a98359f0f10f2d4d89afd4945c4035191a
Lajos Nagy <lajos+hackathon@quix.io>
2024-02-08 20:16:10 +0000

    Edited 'main.py'

--- Input Simulator/main.py
+++ Input Simulator/main.py
@@ -5,14 +5,14 @@ from quixstreams.models.serializers.quix import QuixDeserializer, QuixTimeseries
 
 app = Application.Quix("transformation-v1", auto_offset_reset="latest")
 
-input_topic = app.topic(os.environ["input"], value_deserializer=QuixDeserializer())
+# input_topic = app.topic(os.environ["input"], value_deserializer=QuixDeserializer())
 output_topic = app.topic(os.environ["output"], value_serializer=QuixTimeseriesSerializer())
 
-sdf = app.dataframe(input_topic)
+sdf = app.dataframe(output_topic)
 
 # Here put transformation logic.
 
-sdf = sdf.update(lambda row: print(row))
+# sdf = sdf.update(lambda row: print(row))
 
 sdf = sdf.to_topic(output_topic)
 
