2bc89f1bdea04f66906fa11391ce20bdef7ce8f1
Lajos Nagy <lajos+hackathon@quix.io>
2024-02-09 01:20:09 +0000

    Edited 'main.py'

--- Input Simulator/main.py
+++ Input Simulator/main.py
@@ -13,11 +13,10 @@ from quixstreams import Application
 from quixstreams.platforms.quix import QuixKafkaConfigsBuilder, TopicCreationConfigs
 
 
-app = Application.Quix()
+app = Application.Quix(str(uuid.uuid4()))
 
 output_topic = app.topic(os.environ["output"], value_serializer="json")
 
-consumer_group = app.consumer_group("qlix_raw")
 
 def generate_random_choice(choices):
     return random.choice(choices)
